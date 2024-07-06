d92ed9437fa52a5ed71f99532c715a2d810a7c1f
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-07-06 17:27:11 +0300

    Mpesa promotion

--- mpesa_promotion/mpesa_promotion/asgi.py
+++ mpesa_promotion/mpesa_promotion/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mpesa_daraja project.
+ASGI config for mpesa_promotion project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mpesa_daraja.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mpesa_promotion.settings')
 
 application = get_asgi_application()
